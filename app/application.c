274184ec643e0f1dc443cf4df8339e41323aa3cb
Pavel Hübner <pavel.hubner@bigclown.com>
2017-02-17 13:55:13 +0100

    Default example is led blink; Update submodule

--- app/application.c
+++ app/application.c
@@ -1,5 +1,4 @@
 #include <application.h>
-#include <usb_talk.h>
 
 // LED instance
 bc_led_t led;
@@ -9,22 +8,13 @@ bc_button_t button;
 
 void application_init(void)
 {
-    //usb_talk_init();
-
-    bc_module_power_init();
-    bc_module_power_led_strip_test();
-
     // Initialize LED
     bc_led_init(&led, BC_GPIO_LED, false, false);
+    bc_led_set_mode(&led, BC_LED_MODE_ON);
 
     // Initialize button
     bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
     bc_button_set_event_handler(&button, button_event_handler, NULL);
-
-    // Initialize radio
-    bc_radio_init();
-    bc_radio_set_event_handler(radio_event_handler, NULL);
-    bc_radio_listen();
 }
 
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
@@ -34,36 +24,6 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
 
     if (event == BC_BUTTON_EVENT_PRESS)
     {
-        bc_led_pulse(&led, 100);
-
-        static uint16_t event_count = 0;
-
-        bc_radio_pub_push_button(&event_count);
-
-        event_count++;
+        bc_led_set_mode(&led, BC_LED_MODE_TOGGLE);
     }
-    else if (event == BC_BUTTON_EVENT_HOLD)
-    {
-        bc_radio_enroll_to_gateway();
-        bc_radio_enrollment_start();
-        bc_led_set_mode(&led, BC_LED_MODE_BLINK_FAST);
-    }
-}
-
-void radio_event_handler(bc_radio_event_t event, void *event_param)
-{
-    (void) event_param;
-
-    if (event == BC_RADIO_EVENT_PAIR_SUCCESS)
-    {
-        bc_led_pulse(&led, 1000);
-        bc_led_set_mode(&led, BC_LED_MODE_OFF);
-    }
-}
-
-void bc_radio_on_push_button(uint16_t *event_count)
-{
-    (void) event_count;
-
-    bc_led_pulse(&led, 100);
 }
--- app/application.h
+++ app/application.h
@@ -7,6 +7,4 @@
 
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param);
 
-void radio_event_handler(bc_radio_event_t event, void *event_param);
-
 #endif // _APPLICATION_H
